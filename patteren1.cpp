6b53b76c5276a1ad25f8d82aebc56d13933d2c73
pragya sah <2pragyasah@gmail.com>
2024-08-27 22:34:26 +0530

    day7

---
+++ patteren1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using std::cout;
+
+int main(){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+           cout<<"* ";
+        }
+        cout<<"\n";
+    }
+}
